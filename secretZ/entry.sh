6ff79af142346fc4acd52148eb10f07f13b97825
sinegar <peter@perenyi.net>
2021-12-27 12:25:35 +0000

    split secretz and configz into two containers

--- secretZ/entry.sh
+++ secretZ/entry.sh
@@ -5,7 +5,11 @@ tar cf - -C /configZ --exclude=\*.tpl *Z | tar xvf - -C /
 
 echo $BALENA_DEVICE_UUID > /configZ/roleid
 
+vault agent -config /configZ/secretz.hcl 
 
-vault agent -config /configZ/agent.hcl 
+export VAULT_ADDR=http://secretz:8200 
+export VAULT_API_ADDR=http://secretz:8200 
 
-sh
+echo start vault server
+exec docker-entrypoint.sh server
+
